71cf1936b738ffeb431f6b625ce7f61b7ee68b15
Mikhael D'Amato <mikhael@devtag.com.br>
2021-08-22 21:46:43 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+<img width="64" src="./assets/img/logo_container_view.svg">
+
+![Badge](https://img.shields.io/badge/Container_View-BlueJayGM-%234242c2?style=for-the-badge&logo=ghost)
+
+#Container View
+
+A pattern for moving ideas. Based on a small development area, Container View goal is to instigate the programmer to create a new component, being limited to a pre-defined container.
+
+##🚩 To Begin
+- Step 1: Clone the project → [containerview-clone]("https://github.com/BlueJayGM")
+
+- Step 2: Open in a text editor or IDE
+- Step 3: Use your html and css skills and make a new component for your projects
+
+###By [BlueJayGM]("https://github.com/BlueJayGM") 
---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+var container = window.document.querySelector("div.container");
+var component = window.document.querySelector("button.btn");
+var logo = window.document.querySelector("div.logo");
+
+component.addEventListener("click", onSuccess);
+
+logo.addEventListener("click", () => {
+	window.open("https://github.com/BlueJayGM", "_blank");
+});
+
+function onSuccess(event) {
+	const btnSuccess = event.target || event.srcElement;
+	const classList = btnSuccess.classList;
+	classList.toggle("success");
+	btnSuccess.innerText = classList.value.includes("success") ? "Success" : "Your Component";
+
+	if (classList.value.includes("success")) {
+		onCallBalls();
+	}
+}
+
+// Call all balls
+function onCallBalls() {
+	let width = container.clientWidth;
+	const ball = document.createElement("span");
+	ball.classList.add("ball");
+
+	const colors = [
+		"var(--red)",
+		"var(--blue)",
+		"var(--green)",
+		"var(--red-light)",
+		"var(--blue-light)",
+		"var(--green-light)",
+	];
+
+	let seed = Math.abs(Math.floor(Math.random() * width) - ball.clientWidth);
+
+	ball.style.animationDelay = `${(Math.random() * 2) / 10}s`;
+	ball.style.left = `${seed}px`;
+	ball.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+
+	ball.addEventListener("animationend", () => {
+		container.removeChild(ball);
+	});
+
+	container.appendChild(ball);
+}
+
+// Calls all balls while the button is on success
+setInterval(() => {
+	if (component && component.classList.value.includes("success")) {
+		onCallBalls();
+	}
+}, 100);
